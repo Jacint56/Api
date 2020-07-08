74fe389a0ac417d69aa7d06940d4e1fb0eb0a477
Chon <chon.h.tailor@gmail.com>
2020-07-08 11:37:28 +0200

    Másik is fel van rakva

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => ['dev' => true],
 ];
