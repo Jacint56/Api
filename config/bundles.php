82dc3c0532e6d868ae97ea474325d09395edaeca
Chon <chon.h.tailor@gmail.com>
2020-07-21 15:03:39 +0200

    Composer updateelve

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Trikoder\Bundle\OAuth2Bundle\TrikoderOAuth2Bundle::class => ['all' => true],
 ];
